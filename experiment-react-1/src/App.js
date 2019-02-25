ff6ee9c017e890a4a46a614c70d61273bb1d11da
Zeljko Djuric <zeljko94@gmail.com>
2019-02-25 20:22:54 +0100

    Implemented getBones method and added constructor

--- experiment-react-1/src/App.js
+++ experiment-react-1/src/App.js
@@ -3,6 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(args){
+    super(args);
+    this.state = {
+      bones: []
+    }
+  }
+
+  componentDidMount(){
+    this.getBones()
+  }
+
+  getBones = () => {
+    fetch('https://slashtheapidog.com/api/bones/').then(raw_data => raw_data.json()
+      .then(data => {
+        this.setState({bones: data.data})
+        console.log(data.data)
+      }))
+  }
+
   render() {
     return (
       <div className="App">
