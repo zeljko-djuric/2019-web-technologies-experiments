95eb51bcce53aaf25937dc944432419e3f92677f
Zeljko Djuric <zeljko94@gmail.com>
2019-03-12 20:14:44 +0100

    Added states and their initial values and made get request to analyze incoming data

--- experiment-react-2/src/App.js
+++ experiment-react-2/src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(args){
+    super(args);
+    this.state = {
+      city: '',
+      temp: 0,
+      temp_min: 0,
+      temp_max: 0,
+      icon_code: '',
+      weather: '',
+      weather_description: ''
+    }
+  }
+
+  componentDidMount(){
+    this.getWeather()
+  }
+
+  getWeather = () => {
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=1e0178e320afb8023ce608acbcfea97c')
+      .then(raw_data => raw_data.json()).then(data => console.log(data))
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
       </div>
     );
   }
