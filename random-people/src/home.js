f2ea126b403bc86ad05ed6a4bfebc3f27b2ad590
Zeljko Djuric <zeljko94@gmail.com>
2019-09-23 22:41:37 +0200

    Added home component

---
+++ random-people/src/home.js
@@ -0,0 +1,53 @@
+//home.js
+import React from "react";
+import {
+  ScrollView,
+  ActivityIndicator,
+  StyleSheet
+} from "react-native";
+
+class Home extends React.Component {
+  state = {
+    users: [],
+    loading: true
+  };
+  componentDidMount() {
+    this.getUsers();
+}
+
+async getUsers() {
+    const res = await fetch("https://randomuser.me/api/?results=20");
+    const { results } = await res.json();
+    this.setState({ users: [...results], loading: false });
+}
+
+  render() {
+    return (
+      <ScrollView
+        noSpacer={true}
+        noScroll={true}
+        style={styles.container}
+      >
+       <ActivityIndicator
+            style={[styles.centering, styles.gray]}
+            color="#ff8179"
+            size="large"
+          />
+      </ScrollView>
+    );
+  }
+}
+
+var styles = StyleSheet.create({
+  container: {
+    backgroundColor: "whitesmoke"
+  },
+  centering: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+    height: '100vh'
+  },
+});
+
+export default Home;
