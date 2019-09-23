66c5eb1fb984eab0905ba6abd4a9021e6d2255b5
Zeljko Djuric <zeljko94@gmail.com>
2019-09-23 22:42:21 +0200

    Updated App.js component

--- random-people/src/App.js
+++ random-people/src/App.js
@@ -1,27 +1,27 @@
- import React from 'react';
-    import {
-      AppRegistry,
-      StyleSheet,
-      View,
-    } from 'react-native';
-    import Home from './home'
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  View,
+} from 'react-native';
+import Home from './home'
 
-    class App extends React.Component {
-      render() {
-        return (
-          <View style={styles.appContainer}>
-            <Home />
-          </View>
-        );
-      }
-    }
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.appContainer}>
+        <Home />
+      </View>
+    );
+  }
+}
 
-    const styles = StyleSheet.create({
-      appContainer: {
-        flex: 1,
-      },
-    });
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+  },
+});
 
-    AppRegistry.registerComponent('App', () => App);
+AppRegistry.registerComponent('App', () => App);
 
-    export default App;
+export default App;
